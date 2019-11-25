fc71420b95c20329fbe226b323829e8e20b18b83
Faizaan Chishtie <faizchishtie@gmail.com>
2019-11-25 15:04:04 -0500

    initial commit

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: 'London After Midnight',
-  shortName: 'LAM',
-  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://gatsby-lam.vaporwavy.io', // Domain of your site without prefix!
+  name: 'uoDrone Website',
+  shortName: 'uoDrone',
+  description: 'uoDrone Website',
+  url: 'https://uodrone.com', // Domain of your site without prefix!
   prefix: '/',
-  author: 'Vaporwavy',
+  author: 'Faizaan Chishtie',
   twitter: ''
 }
